600ef4e2aec1b071574e5e9b2396c32ce68632a7
Matt Lacey <matt@mrlacey.co.uk>
2024-06-17 22:23:27 +0100

    add basic error log break in classifier

--- Vsix/XkdlPoc/Classifier/XkdlClassifier.cs
+++ Vsix/XkdlPoc/Classifier/XkdlClassifier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.VisualStudio.Language.StandardClassification;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
@@ -33,88 +34,100 @@ internal class XkdlClassifier : IClassifier
 	{
 		var list = new List<ClassificationSpan>();
 
-		// TODO: improve error handling
-
-		var text = span.GetText();
-
-		var whitespace = text.Substring(0, text.Length - text.TrimStart().Length);
-		bool endsWithPunctuation = text.Trim().EndsWith("\\") || text.Trim().EndsWith("{") || text.Trim().EndsWith("}");
-
-		// TODO: Also handle quotes
-
-		//var rawTextParts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-		var textParts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-		//var textPartsList = new List<string>();
-
-		//var inQuote = false;
-
-		//for (int i = 0; i < text.Length; i++)
-		//{
-		//	if (!inQuote)
-		//	{
-		//		if (text[i])
-		//	}
-		//	else
-		//	{
-		//		if (text[i])
-		//		{ }
-		//	}
-		//}
-
-		//var textParts = textPartsList.ToArray();
-
-		if (textParts.Length >= 1)
+		try
 		{
-			list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start, whitespace.Length), classificationDefault));
+			var text = span.GetText();
 
-			for (int i = 0; i < textParts.Length - (endsWithPunctuation ? 1 : 0); i++)
+			var whitespace = text.Substring(0, text.Length - text.TrimStart().Length);
+			bool endsWithPunctuation = text.Trim().EndsWith("\\") || text.Trim().EndsWith("{") || text.Trim().EndsWith("}");
+
+			// TODO: Also handle quotes
+
+			//var rawTextParts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			var textParts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+			//var textPartsList = new List<string>();
+
+			//var inQuote = false;
+
+			//for (int i = 0; i < text.Length; i++)
+			//{
+			//	if (!inQuote)
+			//	{
+			//		if (text[i])
+			//	}
+			//	else
+			//	{
+			//		if (text[i])
+			//		{ }
+			//	}
+			//}
+
+			//var textParts = textPartsList.ToArray();
+
+			if (textParts.Length >= 1)
 			{
-				if (!textParts[i].Contains("="))
-				{
-					if (endsWithPunctuation || textParts.Length > 1)
-					{
-						list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationTypeName));
-					}
-					else
-					{
-						list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationAttribute));
-					}
-				}
-				else
-				{
-					if (textParts[i].StartsWith("/-"))
-					{
-						list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationComment));
-					}
-					else
-					{
-						// Manually split on the first equals so don't have to account for equals signs in the attribute (embedded C#)
-						var equalsIndex = textParts[i].IndexOf("=");
-						var attributePart1 = textParts[i].Substring(0, equalsIndex);
-						var attributePart2 = textParts[i].Substring(equalsIndex + 1);
+				list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start, whitespace.Length), classificationDefault));
 
-						list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), attributePart1.Length), classificationAttribute));
-						list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length, 1), classificationPunctuation));
-
-						if (attributePart2.StartsWith("\"@") && attributePart2.EndsWith("@\""))
+				for (int i = 0; i < textParts.Length - (endsWithPunctuation ? 1 : 0); i++)
+				{
+					if (!textParts[i].Contains("="))
+					{
+						if (endsWithPunctuation || textParts.Length > 1)
 						{
-							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 1, 1), classificationPunctuation));
-							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 2, attributePart2.Length - 2), classificationCSharp));
-							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 2 + attributePart2.Length - 2, 1), classificationPunctuation));
+							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationTypeName));
 						}
 						else
 						{
-							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 1, attributePart2.Length), classificationAttributeValue));
+							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationAttribute));
+						}
+					}
+					else
+					{
+						if (textParts[i].StartsWith("/-"))
+						{
+							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), textParts[i].Length), classificationComment));
+						}
+						else
+						{
+							// Manually split on the first equals so don't have to account for equals signs in the attribute (embedded C#)
+							var equalsIndex = textParts[i].IndexOf("=");
+							var attributePart1 = textParts[i].Substring(0, equalsIndex);
+							var attributePart2 = textParts[i].Substring(equalsIndex + 1);
+
+							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]), attributePart1.Length), classificationAttribute));
+							list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length, 1), classificationPunctuation));
+
+							if (attributePart2.StartsWith("\"@") && attributePart2.EndsWith("@\""))
+							{
+								list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 1, 1), classificationPunctuation));
+								list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 2, attributePart2.Length - 2), classificationCSharp));
+								list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 2 + attributePart2.Length - 2, 1), classificationPunctuation));
+							}
+							else
+							{
+								list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, span.Start + text.IndexOf(textParts[i]) + attributePart1.Length + 1, attributePart2.Length), classificationAttributeValue));
+							}
 						}
 					}
 				}
 			}
-		}
 
-		if (endsWithPunctuation)
+			if (endsWithPunctuation)
+			{
+				list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, text.TrimEnd().Length - 1, 1), classificationPunctuation));
+			}
+		}
+		catch (Exception exc)
 		{
-			list.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, text.TrimEnd().Length - 1, 1), classificationPunctuation));
+			Debug.WriteLine(exc);
+			Debug.WriteLine(exc.Message);
+			Debug.WriteLine(exc.StackTrace);
+
+			if (Debugger.IsAttached)
+			{
+				Debugger.Break();
+			}
 		}
 
 		return list;
